926244889ebb30a37eb65bc348e7bfeb16ffbdd2
Silikhe <silaswans@gmail.com>
2021-07-17 18:35:23 +0300

    first commit

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent implements OnInit {
+
+  navbarfixed:boolean = false;
+  constructor() {}
+
+  ngOnInit(): void {
+   
+  }
+
+  @HostListener('window:scroll', ['$event']) onscroll(){
+    if(window.scrollY > 250){
+      this.navbarfixed = true
+    }else{
+      this.navbarfixed = false
+    }
+  }
+}
