cc08eeb3aa51a9eaee272b95f02f33c4fc930d8b
Silikhe <silaswans@gmail.com>
2021-07-20 18:32:04 +0300

    (feature):user sign up

--- src/app/models/services/quotes.service.ts
+++ src/app/models/services/quotes.service.ts
@@ -66,8 +66,8 @@ export class QuotesService {
     })
   }
 
-  async sighin(email: string, password: string) {
-    await this.firebaseAuth.signInWithEmailAndPassword(email, password).then(res=>{
+  async sighup(email: string, password: string) {
+    await this.firebaseAuth.createUserWithEmailAndPassword(email, password).then(res=>{
       this.isLoggedIn = true
       localStorage.setItem('user',JSON.stringify(res.user))
     })
