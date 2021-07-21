57a81be066680dd44b52d56253a2e376153649de
Silikhe <silaswans@gmail.com>
2021-07-21 10:30:29 +0300

    Auto-generated commit [ci skip]

--- src/app/pipes/time-d.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimeDPipe } from './time-d.pipe';
-
-describe('TimeDPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimeDPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
