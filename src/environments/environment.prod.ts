9489a4af8d7d5fbe2af0a3aaeff7d4a31918c710
Silikhe <silaswans@gmail.com>
2021-07-21 04:05:15 +0300

    bug(images):fix images not displaying

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDY7h7zNKoinHO72WcJ2cso6nZD3x01POs",
+    authDomain: "angular-quote.firebaseapp.com",
+    projectId: "angular-quote",
+    storageBucket: "angular-quote.appspot.com",
+    messagingSenderId: "111644612898",
+    appId: "1:111644612898:web:59023ca49e078555c3ad7b"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyDY7h7zNKoinHO72WcJ2cso6nZD3x01POs",
     authDomain: "angular-quote.firebaseapp.com",
